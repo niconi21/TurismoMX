f8a0c512c98ff497f61b57518d8bd76825287839
Nicolas Moreno Duran <morenodurann@gmail.com>
2021-11-18 19:05:00 -0600

    Diseño de interfaz

--- app/src/main/java/com/niconi21/turismoargentina/pages/usuario/PerfilFragment.java
+++ app/src/main/java/com/niconi21/turismoargentina/pages/usuario/PerfilFragment.java
@@ -1,66 +1,66 @@
 package com.niconi21.turismoargentina.pages.usuario;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.snackbar.Snackbar;
 import com.niconi21.turismoargentina.R;
+import com.niconi21.turismoargentina.tools.Mensajes;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PerfilFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class PerfilFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public PerfilFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PerfilFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PerfilFragment newInstance(String param1, String param2) {
-        PerfilFragment fragment = new PerfilFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_perfil, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnCambiarClave = view.findViewById(R.id.actualizarClavePerfil);
+        btnCambiarClave.setOnClickListener(v -> {
+            this.cambiarClave(v);
+        });
+    }
+
+    public void cambiarClave(View view) {
+        MaterialAlertDialogBuilder dialogBuilder = new MaterialAlertDialogBuilder(getContext());
+        dialogBuilder.setTitle("Cambiar contraseña");
+        dialogBuilder.setMessage("Escribe tu contraseña actual y posteriormente la nueva contraseña");
+
+        dialogBuilder.setPositiveButton("Cambiar contraseña", (dialog, which) -> {
+            Toast.makeText(getContext(), "Contraseña actualizada", Toast.LENGTH_LONG).show();
+        });
+        dialogBuilder.setNegativeButton("Cancelar", (dialog, which) -> {
+            Toast.makeText(getContext(), "Contraseña no actualizada", Toast.LENGTH_LONG).show();
+        });
+
+        dialogBuilder.create();
+        dialogBuilder.show();
+//        // 1. Instantiate an <code><a href="/reference/android/app/AlertDialog.Builder.html">AlertDialog.Builder</a></code> with its constructor
+//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//
+//// 2. Chain together various setter methods to set the dialog characteristics
+//        builder.setMessage("Mensaje")
+//                .setTitle("Titulo");
+//
+//// 3. Get the <code><a href="/reference/android/app/AlertDialog.html">AlertDialog</a></code> from <code><a href="/reference/android/app/AlertDialog.Builder.html#create()">create()</a></code>
+//        AlertDialog dialog = builder.create();
+//        dialog.show();
     }
 }
